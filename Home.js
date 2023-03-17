3433b5fb356efa12a87ac794a94b51edf66da7f6
Offor Osinachi Winner <offorwinner@gmail.com>
2023-03-17 02:01:41 +0100

    Copied from the duplicate files on hard disk

--- Home.js
+++ Home.js
@@ -1,1 +1,4 @@
-https://github.com/Ossynachi/The-V-I-Hub/blob/undefined/Home.html
+//https://github.com/Ossynachi/The-V-I-Hub/blob/undefined/Home.html
+function showMenu()  {
+    document.getElementById('site-nav').classList.toggle('is-open')
+}  
