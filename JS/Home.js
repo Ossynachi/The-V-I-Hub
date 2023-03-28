858d9af39e1fce5caccae9fbbf415e13050d6c6f
Offor Osinachi Winner <offorwinner@gmail.com>
2023-03-28 01:55:23 +0100

    Test Files / Mayor"s Help
    I added test files , Started working on the drop down menu

--- JS/Home.js
+++ JS/Home.js
@@ -1,4 +1,5 @@
 //https://github.com/Ossynachi/The-V-I-Hub/blob/undefined/Home.html
 function showMenu()  {
-    document.getElementById('site-nav').classList.toggle('is-open')
+    document.getElementById('site-nav').classList.toggle('is-open');
 }  
+
---
+++ Test Files/JS/Home.js
@@ -0,0 +1,20 @@
+//https://github.com/Ossynachi/The-V-I-Hub/blob/undefined/Home.html
+function showMenu()  {
+    document.getElementById('site-nav').classList.toggle('is-open');
+}  
+
+
+.join-drop #bg a{
+    color: white;
+ }
+ .join-drop #outline:hover #bg{
+    background-color: transparent;
+    border-radius: 5px;
+    outline: 1px solid #222;
+ }
+ .join-drop #outline:hover #bg a{
+    color: black;
+ }
+ .join-drop #bg:hover a{
+     color: #000;
+ }
